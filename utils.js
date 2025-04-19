0f0f3bbb2e03804632a2049dbe2f42eaabb05068
EhogGawlic <ethang0227@gmail.com>
2025-04-19 07:45:47 -0700

    e

--- utils.js
+++ utils.js
@@ -327,7 +327,7 @@ function encode(){
     return str
 }
 function testEncode(){
-    const buf = new ArrayBuffer(lines.length*73+fans.length*48+valves.length*16+tcans.length*8)
+    const buf = new ArrayBuffer(lines.length*73+fans.length*48+valves.length*16+tcans.length*8+8)
     const view = new DataView(buf)
     view.setUint16(0, lines.length)
     view.setUint16(2, fans.length)
