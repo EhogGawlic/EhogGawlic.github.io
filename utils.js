c89e6c2d46a46ff29ef53b8ef3cf189c6b53d055
EhogGawlic <ethang0227@gmail.com>
2026-01-25 20:49:56 -0800

    serverahhhhh

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-const server = "https://sauce-medicaid-descriptions-aerospace.trycloudflare.com/"
+const server = "https://ocean-interim-hurricane-administration.trycloudflare.com/"
 let token = sessionStorage.getItem('accessToken') || ''
 
 Float32Array.prototype.push = function(...items){
